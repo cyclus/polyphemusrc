edc8211a628cf7c2fa96533aa6d5c3df835f24b4
Anthony Scopatz <scopatz@gmail.com>
2014-01-14 11:10:51 -0600

    initial cyclus rc repo for polyphemus

---
+++ cyclusrc.py
@@ -0,0 +1,14 @@
+server_name = server_url = 'cyclus-ci.fuelcycle.org'
+port = 80
+
+github_owner = 'cyclus'
+github_repo = 'cyclus'
+github_user = 'cyclus-ci'
+github_credentials = '/root/polyphemusrc/gh.cred'
+
+batlab_user = 'cyclusci'
+batlab_scripts_url = 'https://github.com/cyclus/ciclus/archive/master.zip'
+batlab_fetch_file = 'fetch/cyclus.git'
+batlab_run_spec = 'cyclus.fast.run-spec'
+batlab_submit_cmd = './submit.sh'
+
