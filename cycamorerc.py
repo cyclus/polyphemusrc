f8353cc555ea2c5204796589548214b0bb13bc7d
Anthony Scopatz <scopatz@gmail.com>
2014-01-14 20:08:04 +0000

    added cycamore rc

---
+++ cycamorerc.py
@@ -0,0 +1,18 @@
+server_name = server_url = 'cycamore-ci.fuelcycle.org'
+port = 80
+
+# GitHub setting
+github_owner = 'cyclus'
+github_repo = 'cycamore'
+github_user = 'cyclus-ci'
+github_credentials = '/root/polyphemusrc/gh.cred'
+
+# BaTLab setting
+batlab_user = 'cyclusci'
+batlab_scripts_url = 'https://github.com/cyclus/ciclus/archive/master.zip'
+batlab_fetch_file = 'fetch/cycamore.git'
+batlab_run_spec = 'cycamore.fast.run-spec'
+batlab_submit_cmd = './submit.sh'
+
+# Apache settings
+log_dir = '/root/polyphemusrc'
