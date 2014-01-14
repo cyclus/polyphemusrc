5b5bce1fe2ba0ee352e9702d261a30c1b0571f6c
Anthony Scopatz <scopatz@gmail.com>
2014-01-14 19:58:00 +0000

    workign version of rc

--- cyclusrc.py
+++ cyclusrc.py
@@ -1,14 +1,18 @@
-appname = server_name = server_url = 'cyclus-ci.fuelcycle.org'
-port = 8080
+server_name = server_url = 'cyclus-ci.fuelcycle.org'
+port = 80
 
+# GitHub setting
 github_owner = 'cyclus'
 github_repo = 'cyclus'
 github_user = 'cyclus-ci'
 github_credentials = '/root/polyphemusrc/gh.cred'
 
+# BaTLab setting
 batlab_user = 'cyclusci'
 batlab_scripts_url = 'https://github.com/cyclus/ciclus/archive/master.zip'
 batlab_fetch_file = 'fetch/cyclus.git'
 batlab_run_spec = 'cyclus.fast.run-spec'
 batlab_submit_cmd = './submit.sh'
 
+# Apache settings
+log_dir = '/root/polyphemusrc'
