07e98085c4e89c89385a944703dc92681e1eb07a
Anthony Scopatz <scopatz@gmail.com>
2014-01-17 06:43:15 +0000

    new versions of job_cache

--- cyclusrc.py
+++ cyclusrc.py
@@ -13,6 +13,7 @@ batlab_scripts_url = 'https://github.com/cyclus/ciclus/archive/master.zip'
 batlab_fetch_file = 'fetch/cyclus.git'
 batlab_run_spec = 'cyclus.fast.run-spec'
 batlab_submit_cmd = './submit.sh'
+batlab_jobs_cache = '/var/www/cyclus-ci.fuelcycle.org/jobs.cache'
 
 # Apache settings
 log_dir = '/root/polyphemusrc'
