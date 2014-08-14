39c92bca9fe6637de58008b56d77ed0d2664e15a
Zach Welch <zwelch@wisc.edu>
2014-08-14 13:13:04 -0500

    add conda-specific rc files

---
+++ cycamorerc.conda.py
@@ -0,0 +1,20 @@
+server_name = server_url = 'cycamore-ci.fuelcycle.org'
+port = 80
+
+# GitHub setting
+github_owner = 'cyclus'
+github_repo = 'cycamore'
+github_user = 'cyclus-ci'
+github_credentials = '/root/polyphemusrc/gh.cred'
+
+# BaTLab setting
+batlab_user = 'cyclusci'
+batlab_scripts_url = 'https://github.com/cyclus/ciclus/archive/master.zip'
+batlab_fetch_file = 'fetch/cycamore.git'
+batlab_run_spec = 'cycamore.fast.run-spec'
+batlab_submit_cmd = './submit.sh'
+batlab_jobs_cache = '/var/www/cycamore-ci.fuelcycle.org/jobs.cache'
+batlab_build_type = 'conda'
+
+# Apache settings
+log_dir = '/root/polyphemusrc'
---
+++ cyclusrc.conda.py
@@ -0,0 +1,20 @@
+server_name = server_url = 'cyclus-ci.fuelcycle.org'
+port = 80
+
+# GitHub setting
+github_owner = 'cyclus'
+github_repo = 'cyclus'
+github_user = 'cyclus-ci'
+github_credentials = '/root/polyphemusrc/gh.cred'
+
+# BaTLab setting
+batlab_user = 'cyclusci'
+batlab_scripts_url = 'https://github.com/cyclus/ciclus/archive/master.zip'
+batlab_fetch_file = 'fetch/cyclus.git'
+batlab_run_spec = 'cyclus.fast.run-spec'
+batlab_submit_cmd = './submit.sh'
+batlab_jobs_cache = '/var/www/cyclus-ci.fuelcycle.org/jobs.cache'
+batlab_build_type = 'conda'   
+
+# Apache settings
+log_dir = '/root/polyphemusrc'
